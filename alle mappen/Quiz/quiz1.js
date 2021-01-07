25b56d3e072b8ec2825aa5671b8b5056fac910b5
Alex Marquez <marquezworld@gmail.com>
2021-01-07 10:58:26 +0100

    Quiz toegevoegd

---
+++ alle mappen/Quiz/quiz1.js
@@ -0,0 +1,77 @@
+const quiz1 =
+{
+    "quizID": "Museum 1",
+    "quizMetaData":
+    {
+        "title": "Lisser Art Museum",
+        "imageURI": "https://www.lamlisse.nl/wp-content/uploads/2018/03/LAM-Lisse-logo-white.svg"
+
+    },
+    "quizContent":		
+		[
+			{
+				"question": "In welk jaartal is het Lam museum geopend?",
+				"answers": [{
+						"answer": "2019",
+						"feedback": true
+					},
+					{
+						"answer": "2018",
+						"feedback": false
+					},
+					{
+						"answer": "2017",
+						"feedback": false
+					},
+					{
+						"answer": "2016",
+						"feedback": false
+					},
+					{
+						"answer": "2015",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "LAM – Wie heeft het Lam museum opgericht?",
+				"answers": [{
+						"answer": "Rein Wolfs",
+						"feedback": false
+					},
+					{
+						"answer": "Jan van den Broek",
+						"feedback": true
+					},
+					{
+						"answer": "Sophia Adriana de Bruyn",
+						"feedback": false
+					},
+					{
+						"answer": "Joop van Caldenborgh",
+						"feedback": false
+					}
+				]
+			},
+			{
+				"question": "wat voor type is het Lam Museam: ?",
+				"answers": [{
+						"answer": "Beelden Museam",
+						"feedback": false
+					},
+					{
+						"answer": "Wetenshap museum",
+						"feedback": false
+					},
+					{
+						"answer": "Schilderijen museum",
+						"feedback": false
+					},
+					{
+						"answer": "Kunstmuseum",
+						"feedback": true
+					}
+				]
+			}
+		]
+	}
